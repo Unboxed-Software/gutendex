4fdd2e9f33e892d5b00fc82cb1097958c6e4b96d
Martin <mbuxmann@outlook.com>
2025-06-12 12:40:10 +0200

    .

--- gutendex/urls.py
+++ gutendex/urls.py
@@ -10,5 +10,6 @@ router = routers.DefaultRouter()
 router.register(r'books', views.BookViewSet)
 
 urlpatterns = [
+    re_path(r'^$', TemplateView.as_view(template_name='home.html')),
     re_path(r'^', include(router.urls)),
 ]
